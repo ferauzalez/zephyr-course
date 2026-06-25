15d2af87186491553cbcf5bc9533c263289e52ba
Ferna_net <ferauzalez@gmail.com>
2026-06-25 16:28:09 -0300

    add another_board and use an external LED

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,12 +1,18 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
+#include <zephyr/init.h>
+#include <stdio.h>
+#define MY_INIT_PRIO 32
+#define SYS_INIT(executeBeforeMain, APPLICATION, MY_INIT_PRIO); 
 #define SLEEP_TIME_MS CONFIG_APP_HEARTBEAT_PERIOD_MS
 #define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
+static int executeBeforeMain(void) {
+    printf("This is a function that executes before main() is called.\n");
+    return 0;
+}
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
